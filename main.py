16c1c4fc7d9877dca877ed1d06d60391e1243c08
master <papachristos.andreas@gmail.com>
2021-03-28 22:12:55 +0300

    create csv from templates

--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import csv
+import csv, glob
 convertor = 0.3527777778
 style = ("sheetwise", "worknturn", "workntumble", "single_sided", "perfector")
 ptTomm = lambda x: int(x * convertor)
 
 
-def readTplFile():
-    with open("/run/media/master/Transcend/Templates/EPIPEDES/man roland 708/70X100_P-F_240X320_S16_Head5mm.tpl", "r") as f:
+def readTplFile(path):
+    with open(path, "r") as f:
         return f.readlines()[9].split(" ")[1:8]
 
 def writeCsv(path):
+
+
     with open(path, "w") as f:
         fileWriter = csv.writer(f, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
         fileWriter.writerow(["width", "Height", "Style", "pages", "pageWidth", "pageHeight"])
+        for file in glob.glob("/run/media/master/Transcend/Templates/EPIPEDES/" + "**/*tpl", recursive=True):
+            x = readTplFile(file)
+            fileWriter.writerow([float(x[0])*convertor, float(x[1])*convertor, style[int(x[4])]])
 if __name__ == '__main__':
     writeCsv("/home/master/test.csv")
